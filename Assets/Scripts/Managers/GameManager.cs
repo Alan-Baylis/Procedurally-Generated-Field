c96856cc1326ef7be97a0a3b9cb679e286142dd0
Porrith Suong <psuong95@gmail.com>
2016-01-02 21:38:59 -0500

    Manager folder added to repo with empty manager scripts.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    
+    public KeyCode startKey = KeyCode.Space; // Button to press that restarts the level.
+    public Maze mazePrefab; 
+    private Maze mazeInstance;
+    
+    private void Start() {
+        BeginGame();
+    }
+    
+    private void Update() {
+        if (Input.GetKey(startKey)) {
+            RestartGame();
+        }
+    }
+    
+    private void BeginGame() {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+    }
+    
+    private void RestartGame() {
+        // Destroy the maze instance and restart the generation
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
