6f5141ffdb37a7170cff8712a75971df5665a81d
Porrith Suong <psuong95@gmail.com>
2016-01-03 00:35:55 -0500

    Adds a method to randomly generate a set of coordinates.

--- Assets/Scripts/Managers/IntVector2.cs
+++ Assets/Scripts/Managers/IntVector2.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public struct IntVector2 {
     public int x, z;
     public IntVector2 (int x, int z) {
--- Assets/Scripts/Managers/Maze.cs
+++ Assets/Scripts/Managers/Maze.cs
@@ -8,7 +8,10 @@ public class Maze : MonoBehaviour {
     public float delayTime = 0.01f;
     private MazeCell[,] cells;
     
-    // Coroutine generates the field progressively.
+    #region Public Methods
+    /// <summary>
+    /// Coroutine generates the field progressively.
+    /// <summary>
     public IEnumerator Generate() {
         cells = new MazeCell[fieldDimension.x, fieldDimension.z];
         for (int i = 0; i < fieldDimension.x; i++) {
@@ -19,6 +22,15 @@ public class Maze : MonoBehaviour {
         }
     }
     
+    /// <summary>
+    /// Returns a struct of two random coordinates.
+    /// </summary>
+    /// <returns>IntVector2 struct with 2 random coordinates</returns>
+    public IntVector2 GetRandomCoordinates() {
+        return new IntVector2(Random.Range(0, fieldDimension.x), Random.Range(0, fieldDimension.z));
+    }
+    #endregion
+    
     private void CreateCell(IntVector2 coordinates) {
         // Create a cell object and snap it to some position
         MazeCell newCell = Instantiate(cell) as MazeCell;
@@ -26,5 +38,5 @@ public class Maze : MonoBehaviour {
         newCell.name = "Maze Cell: " + coordinates.x + " " + coordinates.z;
         newCell.transform.parent = transform;
         newCell.transform.localPosition = new Vector3(coordinates.x - fieldDimension.x * 0.5f + 0.5f, 0f, coordinates.z - fieldDimension.z * 0.5f + 0.5f);
-    }
+    }    
 }
