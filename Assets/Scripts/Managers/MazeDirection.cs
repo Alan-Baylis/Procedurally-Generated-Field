f64dbe3aa3b87abff68ad4b76666beedf7ddc97b
Porrith Suong <psuong95@gmail.com>
2016-01-03 11:05:14 -0500

    Fixes GetRandomValue conversion to return a state in the Enum.

--- Assets/Scripts/Managers/MazeDirection.cs
+++ Assets/Scripts/Managers/MazeDirection.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MazeDirection {
+public enum MazeDirection { North, East, South, West }
+
+public static class MazeDirections {
     
     public const int count = 4; // Defines how many directions there are
-    public static int GetRandomValue {
-        get { return (Random.Range(0, count)); }
+    public static MazeDirection getRandomValue { get { return (MazeDirection)Random.Range(0, count); } } // Returns a random set of integers
+    
+    public static IntVector2 IntToVector2(MazeDirection direction) {
+        return directionVectors[(int)direction];
     }
+    
+    private static IntVector2[] directionVectors = { // Directional Vector reference
+        new IntVector2(0, 1), // North
+        new IntVector2(1, 0), // East
+        new IntVector2(0, -1), // South
+        new IntVector2(-1, 0) // WEst
+    };
+    
 }
