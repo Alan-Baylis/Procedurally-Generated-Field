a38d53460657b25a00f1cc8040b7fae198b267b8
Porrith Suong <psuong95@gmail.com>
2016-01-02 23:57:40 -0500

    Adds operator overloading.

--- Assets/Scripts/Managers/IntVector2.cs
+++ Assets/Scripts/Managers/IntVector2.cs
@@ -7,4 +7,11 @@ public struct IntVector2 {
         this.x = x;
         this.z = z;
     }
+    
+    // Operator overloading, allows the + operator to be used to add 2 vectors.
+    public static IntVector2 operator + (IntVector2 a, IntVector2 b) {
+        a.x += b.x;
+        a.z += b.z;
+        return a;
+    }
 }
