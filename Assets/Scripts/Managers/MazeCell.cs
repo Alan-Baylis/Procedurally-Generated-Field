0a86f4e8400fd21439ec2fb40411665e61f84fd1
Porrith Suong <psuong95@gmail.com>
2016-01-04 21:06:23 -0500

    Adds edge class to be associated with a MazeCell and relative direction of the edge.

--- Assets/Scripts/Managers/MazeCell.cs
+++ Assets/Scripts/Managers/MazeCell.cs
@@ -4,14 +4,31 @@ using System.Collections;
 public class MazeCell : MonoBehaviour {
     
     public IntVector2 coordinates;
+    
+    private MazeCellEdge[] cellEdges;
+    
+    private void Awake() {
+        cellEdges = new MazeCellEdge[MazeDirections.count];
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    #region Public Methods    
+    /// <summary>
+    /// Sets a reference to an edge via the direction.
+    /// </summary>
+    /// <param name="direction">Direction of an edge.</param>
+    /// <param name="edge">MazeCellEdge associated with the direction.</param>
+    public void SetEdge(MazeDirection direction, MazeCellEdge edge) {
+        cellEdges[(int)direction] = edge;
+    }
+    
+    /// <summary>
+    /// Gets an edge of a cell associated with a direction.
+    /// </summary>
+    /// <param name="direction">Relative direction of an edge on a cell.</param>
+    /// <returns>
+    public MazeCellEdge GetEdge(MazeDirection direction) {
+        return cellEdges[(int)direction];
+    }
+    
+    #endregion
 }
---
+++ Assets/Scripts/Managers/MazeCellEdge.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCellEdge : MonoBehaviour {
+    
+    public MazeCell cell, otherCell;
+    public MazeDirection direction;
+    
+    private 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    #region Public Methods
+    /// <summary>
+    /// Registers an edge to a cell.
+    /// </summary>
+    /// <param name="cell">Reference to a MazeCell instantiated.</param>
+    /// <param name="otherCell">Neighbor MazeCell instantiated.</param>
+    /// <param name="direction">Relative location of the edge.</param>
+    public void Initialize(MazeCell cell, MazeCell otherCell, MazeDirection direction) {
+        this.cell = cell;
+        this.otherCell = otherCell;
+        this.direction = direction;
+        cell.SetEdge(direction, this);
+        transform.parent = cell.transform;
+        transform.localPosition = Vector3.zero;
+    }
+    #endregion
+}
